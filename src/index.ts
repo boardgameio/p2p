80e95b3e2ac20eddc933c6abdef8db5a85c8bd38
delucis <swithinbank@gmail.com>
2021-10-06 22:50:03 +0100

    feat(P2PHost): Authenticate clients on registration
    Closes #1

--- src/index.ts
+++ src/index.ts
@@ -100,9 +100,49 @@ class P2PHost {
   }
 
   registerClient(client: Client): void {
+    const isAuthenticated: boolean = this.authenticateClient(client);
+    // If the client failed to authenticate, don’t register it.
+    if (!isAuthenticated) return;
     const { playerID, credentials } = client.metadata;
     this.clients.set(client, client);
     this.master.onConnectionChange(this.matchID, playerID, credentials, true);
+  }
+
+  /**
+   * Store a player’s credentials on initial connection and authenticate them subsequently.
+   * @param client Client to authenticate.
+   * @returns `true` if the client was successfully authenticated, `false` if it wasn’t.
+   */
+  private authenticateClient(client: Client): boolean {
+    const { playerID, credentials } = client.metadata;
+    const { metadata } = this.db.fetch(this.matchID);
+
+    // Spectators provide null/undefined playerIDs and don’t need authenticating.
+    if (
+      playerID === null ||
+      playerID === undefined ||
+      !(+playerID in metadata.players)
+    ) {
+      return true;
+    }
+
+    const existingCredentials = metadata.players[+playerID].credentials;
+
+    // If no credentials exist yet for this player, store those
+    // provided by the connecting client and authenticate.
+    if (!existingCredentials && credentials) {
+      this.db.setMetadata(this.matchID, {
+        ...metadata,
+        players: {
+          ...metadata.players,
+          [+playerID]: { ...metadata.players[+playerID], credentials },
+        },
+      });
+      return true;
+    }
+
+    // If credentials match (or credentials are neither provided nor stored), authenticate.
+    return credentials === existingCredentials;
   }
 
   unregisterClient(client: Client): void {
