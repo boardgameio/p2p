c4e0708f863e74b905c796fa2c8692057521c2d4
delucis <swithinbank@gmail.com>
2021-11-15 18:25:34 +0000

    test: Rewrite tests
    - Use full database instances and new `authenticate` signature.
    - Test authentication without credentials

--- test/authentication.test.ts
+++ test/authentication.test.ts
@@ -1,12 +1,13 @@
-import { Server } from "boardgame.io";
+import type { Server, State } from "boardgame.io";
 import nacl from "tweetnacl";
 import { encodeBase64 } from "tweetnacl-util";
 import { authenticate, signMessage } from "../src/authentication";
+import { P2PDB } from "../src/db";
 import { Client } from "../src/types";
 
 const matchID = "TestMatch";
-const defaultMetadata = { gameName: "TestGame", createdAt: 0, updatedAt: 0 };
 
+/** Generate a base64-encoded public/private key pair. */
 function keys() {
   const keyPair = nacl.sign.keyPair();
   return {
@@ -15,135 +16,125 @@ function keys() {
   };
 }
 
-//[+player1Metadata.playerID]: {id: +player1Metadata.playerID}
-
-test("PlayerID not registered", () => {
-  const player1keyPair = nacl.sign.keyPair();
-  const player1Metadata: Client["metadata"] & { playerID: string } = {
-    playerID: "Player1",
-    credentials: encodeBase64(player1keyPair.publicKey),
-    message: undefined,
-  };
-
-  let serverMetadata: Server.MatchData = { ...defaultMetadata, players: {} };
-
-  const db: any = {
-    setMetadata: (_matchID: string, _serverMetadata: Server.MatchData) => {
-      serverMetadata = _serverMetadata;
+/** Create and pre-populate a database instance. */
+function createDB(matchID: string, numPlayers = 2) {
+  const db = new P2PDB();
+  db.createMatch(matchID, {
+    initialState: {} as State,
+    metadata: {
+      gameName: "TestGame",
+      createdAt: 0,
+      updatedAt: 0,
+      players: Array.from({ length: numPlayers }).reduce(
+        (players: Server.MatchData["players"], _, id) => ({
+          ...players,
+          [id]: { id },
+        }),
+        {}
+      ),
     },
-  };
+  });
+  return db;
+}
 
-  expect(
-    authenticate(matchID, player1Metadata, serverMetadata, db)
-  ).toBeTruthy();
+describe("without credentials", () => {
+  const db = createDB(matchID);
+  const player0Metadata = { playerID: "0", credentials: undefined };
+
+  test("successfully authenticates on first connection", () => {
+    expect(authenticate(matchID, player0Metadata, db)).toBe(true);
+  });
+
+  test("successfully authenticates on subsequent connection", () => {
+    expect(authenticate(matchID, player0Metadata, db)).toBe(true);
+  });
 });
 
-test("PlayerID null", () => {
-  const player1keyPair = nacl.sign.keyPair();
-  const player1Metadata: Client["metadata"] = {
-    playerID: null,
-    credentials: encodeBase64(player1keyPair.publicKey),
-    message: undefined,
-  };
+describe("with public-key encryption", () => {
+  test("successfully authenticates if playerID not registered", () => {
+    const db = createDB(matchID);
+    const player0Keys = keys();
+    const player0Metadata: Client["metadata"] & { playerID: string } = {
+      playerID: "0",
+      credentials: player0Keys.publicKey,
+      message: signMessage("test", player0Keys.privateKey),
+    };
 
-  let serverMetadata: Server.MatchData = { ...defaultMetadata, players: {} };
+    expect(authenticate(matchID, player0Metadata, db)).toBe(true);
+    expect(db.fetch(matchID).metadata.players["0"].credentials).toEqual(
+      player0Keys.publicKey
+    );
+  });
 
-  const db: any = {
-    setMetadata: (_matchID: string, _serverMetadata: Server.MatchData) => {
-      serverMetadata = _serverMetadata;
-    },
-  };
+  test("successfully authenticates after initial registration", () => {
+    const db = createDB(matchID);
+    const player0Keys = keys();
+    const player0Metadata: Client["metadata"] & { playerID: string } = {
+      playerID: "0",
+      credentials: player0Keys.publicKey,
+      message: signMessage("test", player0Keys.privateKey),
+    };
 
-  expect(
-    authenticate(matchID, player1Metadata, serverMetadata, db)
-  ).toBeTruthy();
-});
+    expect(authenticate(matchID, player0Metadata, db)).toBe(true);
+    expect(db.fetch(matchID).metadata.players["0"].credentials).toEqual(
+      player0Keys.publicKey
+    );
 
-test("Player1 is registered and Player2 sends wrong message", () => {
-  const player1Keys = keys();
-  const message = signMessage("Test", player1Keys.privateKey);
-  const player1Metadata: Client["metadata"] & { playerID: string } = {
-    playerID: "1",
-    credentials: player1Keys.publicKey,
-    message,
-  };
+    const newMetadata = {
+      ...player0Metadata,
+      message: signMessage("new-connection", player0Keys.privateKey),
+    };
+    expect(authenticate(matchID, newMetadata, db)).toBe(true);
+  });
 
-  const player2Keys = keys();
-  const player2Metadata: Client["metadata"] & { playerID: string } = {
-    playerID: "2",
-    credentials: player2Keys.publicKey,
-    message,
-  };
+  test("successfully authenticates if playerID is null (spectator)", () => {
+    const db = createDB(matchID);
+    const spectatorKeys = keys();
+    const spectatorMetadata: Client["metadata"] = {
+      playerID: null,
+      credentials: spectatorKeys.publicKey,
+    };
+    expect(authenticate(matchID, spectatorMetadata, db)).toBe(true);
+  });
 
-  let serverMetadata: Server.MatchData = {
-    ...defaultMetadata,
-    players: {
-      [+player1Metadata.playerID]: { id: +player1Metadata.playerID },
-      [+player2Metadata.playerID]: { id: +player2Metadata.playerID },
-    },
-  };
+  test("authentication fails if player sends invalid message", () => {
+    const db = createDB(matchID);
+    const clientMetadata = {
+      playerID: "1",
+      credentials: keys().publicKey,
+      message: "unsigned-message",
+    };
+    expect(authenticate(matchID, clientMetadata, db)).toBe(false);
+    expect(db.fetch(matchID).metadata.players["1"].credentials).toBeUndefined();
+  });
 
-  const db: any = {
-    setMetadata: (_matchID: string, _serverMetadata: Server.MatchData) => {
-      serverMetadata = _serverMetadata;
-    },
-  };
+  test("multiple players can successfully authenticate", () => {
+    const db = createDB(matchID);
 
-  expect(
-    serverMetadata.players[+player1Metadata.playerID].credentials
-  ).toBeUndefined();
-  expect(
-    authenticate(matchID, player1Metadata, serverMetadata, db)
-  ).toBeTruthy();
-  expect(serverMetadata.players[+player1Metadata.playerID].credentials).toBe(
-    player1Keys.publicKey
-  );
-  expect(
-    authenticate(matchID, player2Metadata, serverMetadata, db)
-  ).toBeFalsy();
-});
+    const player0Keys = keys();
+    const player0Metadata = {
+      playerID: "0",
+      credentials: player0Keys.publicKey,
+      message: signMessage("Test", player0Keys.privateKey),
+    };
 
-test("Player1 is registered and Player2 sends wrong message", () => {
-  const player1Keys = keys();
-  const message1 = signMessage("Test", player1Keys.privateKey);
-  const player1Metadata: Client["metadata"] & { playerID: string } = {
-    playerID: "1",
-    credentials: player1Keys.publicKey,
-    message: message1,
-  };
+    const player1Keys = keys();
+    const player1Metadata: Client["metadata"] & { playerID: string } = {
+      playerID: "1",
+      credentials: player1Keys.publicKey,
+      message: signMessage("Test", player1Keys.privateKey),
+    };
 
-  const player2Keys = keys();
-  const message2 = signMessage("Test", player2Keys.privateKey);
-  const player2Metadata: Client["metadata"] & { playerID: string } = {
-    playerID: "2",
-    credentials: player2Keys.publicKey,
-    message: message2,
-  };
+    // Player 0 authenticates successfully and their public key is stored.
+    expect(authenticate(matchID, player0Metadata, db)).toBe(true);
+    expect(
+      db.fetch(matchID).metadata.players[+player0Metadata.playerID].credentials
+    ).toBe(player0Keys.publicKey);
 
-  let serverMetadata: Server.MatchData = {
-    ...defaultMetadata,
-    players: {
-      [+player1Metadata.playerID]: { id: +player1Metadata.playerID },
-      [+player2Metadata.playerID]: { id: +player2Metadata.playerID },
-    },
-  };
-
-  const db: any = {
-    setMetadata: (_matchID: string, _serverMetadata: Server.MatchData) => {
-      serverMetadata = _serverMetadata;
-    },
-  };
-
-  expect(
-    serverMetadata.players[+player1Metadata.playerID].credentials
-  ).toBeUndefined();
-  expect(
-    authenticate(matchID, player1Metadata, serverMetadata, db)
-  ).toBeTruthy();
-  expect(serverMetadata.players[+player1Metadata.playerID].credentials).toBe(
-    player1Keys.publicKey
-  );
-  expect(
-    authenticate(matchID, player2Metadata, serverMetadata, db)
-  ).toBeTruthy();
+    // Player 1 authenticates successfully and their public key is stored.
+    expect(authenticate(matchID, player1Metadata, db)).toBe(true);
+    expect(
+      db.fetch(matchID).metadata.players[+player1Metadata.playerID].credentials
+    ).toBe(player1Keys.publicKey);
+  });
 });
