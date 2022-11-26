41e40a71368dfcc356d40e61d9fe12fbf8510509
funfood-berlin <github@funfood-berlin.de>
2022-11-26 16:36:34 +0100

    formatting

--- src/index.ts
+++ src/index.ts
@@ -104,10 +104,9 @@ class P2PTransport extends Transport {
     if (!this.matchID) throw new Error("matchID must be provided");
     // Sanitize host ID for PeerJS: remove any non-alphanumeric characters, trim
     // leading/trailing hyphens/underscores and collapse consecutive hyphens/underscores.
-    return `boardgameio-${this.gameName}-matchid-${this.matchID}`.replace(
-      /([^A-Za-z0-9_-]|^[_-]+|[_-]+$)/g, 
-      ""
-    ).replace(/([_-])[_-]+/g, "$1");
+    return `boardgameio-${this.gameName}-matchid-${this.matchID}`
+      .replace(/([^A-Za-z0-9_-]|^[_-]+|[_-]+$)/g, "")
+      .replace(/([_-])[_-]+/g, "$1");
   }
 
   /** Keep credentials and encryption keys in sync. */
